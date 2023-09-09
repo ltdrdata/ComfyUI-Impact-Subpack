c81a73e4790acc02b97ce653f406e06713ba1e05
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-09-09 14:15:57 +0900

    improve: add skip_download_model

--- install.py
+++ install.py
@@ -12,20 +12,21 @@ model_path = folder_paths.models_dir
 ultralytics_bbox_path = os.path.join(model_path, "ultralytics", "bbox")
 ultralytics_segm_path = os.path.join(model_path, "ultralytics", "segm")
 
-if not os.path.exists(ultralytics_bbox_path):
-    os.makedirs(ultralytics_bbox_path)
+if not os.path.exists(os.path.join(subpack_path, '..', '..', 'skip_download_model')):
+    if not os.path.exists(ultralytics_bbox_path):
+        os.makedirs(ultralytics_bbox_path)
 
-if not os.path.exists(ultralytics_segm_path):
-    os.makedirs(ultralytics_segm_path)
+    if not os.path.exists(ultralytics_segm_path):
+        os.makedirs(ultralytics_segm_path)
 
-if not os.path.exists(os.path.join(ultralytics_bbox_path, "face_yolov8m.pt")):
-    download_url("https://huggingface.co/Bingsu/adetailer/resolve/main/face_yolov8m.pt",
-                 ultralytics_bbox_path)
+    if not os.path.exists(os.path.join(ultralytics_bbox_path, "face_yolov8m.pt")):
+        download_url("https://huggingface.co/Bingsu/adetailer/resolve/main/face_yolov8m.pt",
+                     ultralytics_bbox_path)
 
-if not os.path.exists(os.path.join(ultralytics_bbox_path, "hand_yolov8s.pt")):
-    download_url("https://huggingface.co/Bingsu/adetailer/resolve/main/hand_yolov8s.pt",
-                 ultralytics_bbox_path)
+    if not os.path.exists(os.path.join(ultralytics_bbox_path, "hand_yolov8s.pt")):
+        download_url("https://huggingface.co/Bingsu/adetailer/resolve/main/hand_yolov8s.pt",
+                     ultralytics_bbox_path)
 
-if not os.path.exists(os.path.join(ultralytics_segm_path, "person_yolov8m-seg.pt")):
-    download_url("https://huggingface.co/Bingsu/adetailer/resolve/main/person_yolov8m-seg.pt",
-                 ultralytics_segm_path)
+    if not os.path.exists(os.path.join(ultralytics_segm_path, "person_yolov8m-seg.pt")):
+        download_url("https://huggingface.co/Bingsu/adetailer/resolve/main/person_yolov8m-seg.pt",
+                     ultralytics_segm_path)
